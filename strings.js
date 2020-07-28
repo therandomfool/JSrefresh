bb098bf7ead092dd7c71761961f8ab4eea2eeea3
therandomfool <mddenver@protonmail.com>
2020-07-27 20:13:41 -0700

    temp conversion

---
+++ strings.js
@@ -0,0 +1,3 @@
+let name = 'dfohoufdhoiufdsaoih';
+
+console.log(name);
---
+++ temp-conversion.js
@@ -0,0 +1,10 @@
+let fahr =32;
+
+// calculate celsius and store and print
+
+cel = ((fahr - 32)  / 1.8);
+kel = ((fahr + 459.67) * 5/9);
+
+console.log(fahr + "Fahrenheit");
+console.log(cel + " Celsius");
+console.log(kel + " Kelvin");
